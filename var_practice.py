3075470bf28f77055f629dba033b7951c98980fb
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-23 18:54:08 -0500

    string format method practice

--- var_practice.py
+++ var_practice.py
@@ -14,18 +14,23 @@ print('n/m + 1 =' , n/m + 1, 125, sep='\n')
 x = 2
 y = 3
 result = x/y
+'''
 print('result =', result)
 # format using string format operator %
-'''
+
 print('text' % result) text
 print('    ' % result) escape sequence
 print('%[]' % result) format specification %[flag][width][letter]
     f | float number
     d | whole number
     s | string
-'''
+
 print('result = %f' % result)
 print('result = %.2f' % result)
 print('result = %5.2f' % result)
 print('%d\n%.2f' % (12 , 2.765))
 print('%05d\n%.2f' % (12 , 2.765))
+'''
+# format using string format method
+print('{:.2f}'.format(result))
+print('{:.2f}\n{:d}'.format(result, 124))
