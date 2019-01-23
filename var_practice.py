b947d943e96227cd39c1165f4d4312db68c866a1
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-23 18:39:15 -0500

    further format string operator practice

--- var_practice.py
+++ var_practice.py
@@ -27,5 +27,5 @@ print('%[]' % result) format specification %[flag][width][letter]
 print('result = %f' % result)
 print('result = %.2f' % result)
 print('result = %5.2f' % result)
-
 print('%d\n%.2f' % (12 , 2.765))
+print('%05d\n%.2f' % (12 , 2.765))
