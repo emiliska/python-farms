deea05f3e9f028dbc63c0bd00452b603d6dee550
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-23 17:59:55 -0500

    further seperator practice

--- var_practice.py
+++ var_practice.py
@@ -3,5 +3,7 @@ m=3
 print(n/m)
 print(n/m + 1)
 print('n/m + 1 =' , n/m + 1)
-print('n/m + 1 =' , n/m + 1, sep='#####' )
+#print('n/m + 1 =' , n/m + 1, sep='     ' )
 print('n/m + 1 =' ,'\n' , n/m + 1)
+print('n/m + 1 =' , n/m + 1, sep='\n' )
+print('n/m + 1 =' , n/m + 1, 125, sep='\n' )
