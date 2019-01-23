bf7eefab88bfd4c40e10ba51e197a97a01fce249
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-23 18:25:21 -0500

    format using string format operator %

--- var_practice.py
+++ var_practice.py
@@ -1,3 +1,4 @@
+'''
 n=2
 m=3
 print(n/m)
@@ -6,4 +7,21 @@ print('n/m + 1 =' , n/m + 1)
 #print('n/m + 1 =' , n/m + 1, sep='     ' )
 print('n/m + 1 =' ,'\n' , n/m + 1)
 print('n/m + 1 =' , n/m + 1, sep='\n' )
-print('n/m + 1 =' , n/m + 1, 125, sep='\n' )
+print('n/m + 1 =' , n/m + 1, 125, sep='\n')
+'''
+
+#how to format my output
+x = 2
+y = 3
+result = x/y
+print('result =', result)
+# format using string format operator %
+'''
+print('text' % result) text
+print('    ' % result) escape sequence
+print('%[]' % result) format specification %[flag][width][letter]
+    f | float number
+    d | whole number
+    s | string
+'''
+print('result = %f' % result)
