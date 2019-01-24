c3b3821934a627f9144ee444c8fc50b47cc4f11a
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-24 09:50:04 -0500

    Do part two of lab 1

---
+++ lab1/lab1b.py
@@ -0,0 +1,9 @@
+print('\nProgrammer: NAME', 'Course:     COSC146, Winter 2019', 'Lab#:       1, part#2', 'Due date:   1-16-2019\n', sep='\n')
+
+print('10 - 3 =', 10-3)
+#print('10 / 3 =', 10/3)
+#truncate result to hundredths place X.XX
+print('10 / 3 =','{:.2f}'.format(10/3))
+print('10 * 3 =', 10*3)
+print('10 ** 3 =', 10**3)
+print('(10 + 3) / 2 =', (10+3)/2)
