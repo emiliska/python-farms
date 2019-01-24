78e40c53ae13361fc8e42b62c6a4f8d167484539
emiliska <33560517+emiliska@users.noreply.github.com>
2019-01-23 19:02:32 -0500

    lab1 practice

---
+++ lab1/lab1a.py
@@ -0,0 +1,14 @@
+# Triple-quoted string to output personal information
+print('Programmer: NAME', 'Course:     COSC146, Winter 2019', 'Lab#:       1, part#1', 'Due date:   1-16-2019', sep='\n')
+
+# Print "Roses are Red" to console
+print('\nRoses are Red\n')
+
+# Print Poem with Border
+border = '+++++++++++++++++++++++++++'
+poem   = '+  Roses are red          +\n+  Violets are blue       +\n+  "Chocolate" is better  +\n+ By a \'pound\' or two!!   +' 
+display = border + '\n' + poem + '\n' + border
+print(display)
+
+# Replace '+' with '/'
+print('\n', display.replace("+", "\\"), '\n')
