33fa058283543c34b91189ef11bc24b36102db14
emiliska <33560517+emiliska@users.noreply.github.com>
2019-02-04 17:48:19 -0500

    Show example of an 'if' condition

---
+++ week_3/conditional_statements.py
@@ -0,0 +1,7 @@
+# make a decision using a simple conditional statement
+number = int(input('Enter a whole number: '))
+
+if number > 5:
+    print('number is bigger than 5')
+    print(number + 10)
+print('done')
