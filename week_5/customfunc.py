bf40013ec2136df697c6c43f95d765a8714fcb7e
emiliska <33560517+emiliska@users.noreply.github.com>
2019-02-18 18:49:59 -0500

    move customfunc.py

---
+++ week_5/customfunc.py
@@ -0,0 +1,15 @@
+# define a function that display the output heading
+def output_heading():
+    print('Programmer:        Emily')
+    print('Course:            COSC146')
+    print('Lab#:              0')
+    print('Due Date:          02-19-2019')
+
+#define a function that takes a number and returns that number + 10
+def plus10 (x):
+    value = value + 10
+    return value
+
+#call function print_heading
+output_heading()
+print('done!')
