13b806fd8a7a387033766ebbdfc4e307bca0ace6
emiliska <33560517+emiliska@users.noreply.github.com>
2019-03-18 18:41:34 -0400

    list examples

---
+++ week_9/list.py
@@ -0,0 +1,40 @@
+list1 = []
+list2 = ()
+list3 = list("Hello")
+print(list1, list2, list3)
+
+numbers = [1, 5, 10, 100, 18]
+strings = ['Toyota', 'Honda', 'BMW', 'Chevy', 'Ford']
+mixed = ['Tom', 12, 2.86, 'A']
+nested = ['Hello', [2, 100], 12, 'a']
+print(numbers)
+print(strings)
+print(mixed)
+
+temp = [strings, 11, 12]
+print(temp)
+
+print(len(nested))
+
+# access list elements
+
+print(strings[2])
+print(strings[:3])
+
+# traversing a list
+for elm in mixed:
+    print(elm)
+
+# output list elements that are in odd positions
+for pos in range(5):
+    if pos % 2 == 1:
+        print(numbers[pos])
+
+# do something with a while-loop
+pos = 0
+while pos < 5:
+    if pos % 2 == 1:
+        print(numbers[pos])
+    pos += 1
+
+# list build-in functions: len, max, min, sum
