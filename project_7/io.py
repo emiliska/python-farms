9d9d130357a003719af89320d0f4182bd1772fb3
emiliska <33560517+emiliska@users.noreply.github.com>
2019-03-27 19:08:25 -0400

    finish

--- project_7/io.py
+++ project_7/io.py
@@ -10,6 +10,7 @@ Due date:   March 27, 2019
 ''')
 # ask for and open file
 fname = input('Enter a file name:')
+print()
 fileW = open (fname, 'w')
 # set up a while that iterates 100 times
 count = 0
@@ -25,3 +26,64 @@ for line in fileR:
     print(line, end='')
 fileR.close()
 
+# open file and calculate sum
+fileR = open(fname, 'r')
+total = 0
+count = 0
+avgc = 0
+for line in fileR:
+    total += int(line)
+    if int(line)>15:
+        count += 1
+    avgc += 1
+avgc = total/avgc
+fileR.close()
+print('\nAverage of the numbers in \'', fname,'\': %.1f' % avgc, sep='')
+print('Count of the numbers > 15:', count, '\n')
+
+# open file and append summaries
+fileR = open(fname, 'a')
+fileR.write(str('\nAverage of the numbers in \''+ fname+'\': %.1f' % avgc+'\n'))
+fileR.write('Count of the numbers > 15:'+str(count)+'\n')
+fileR.close()
+
+'''
+PS D:\github\python-farms\project_7> python .\io.py
+
+Programmer: Emily Eubanks
+Course:     COSC 146, Winter 2019
+Lab#:       7
+Due date:   March 27, 2019
+
+Enter a file name:lab7.txt
+
+Contents of the input file 'lab7.txt':
+15
+14
+16
+19
+11
+13
+13
+11
+16
+10
+
+Average of the numbers in 'lab7.txt': 13.8
+Count of the numbers > 15: 3
+
+Copy the contents of 'lab7.txt' and paste it below:
+15
+14
+16
+19
+11
+13
+13
+11
+16
+10
+
+Average of the numbers in 'lab7.txt': 13.8
+Count of the numbers > 15:3
+'''
